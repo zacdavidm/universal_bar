dc566af730fc227812b72c3bc1a37e1fecba8554
ZacDavidM <zacdavidm@gmail.com>
2014-10-26 23:25:16 -0400

    initialize $account_user to empty, remove console log, add in notifications placeholder

--- js/universal-bar.js
+++ js/universal-bar.js
@@ -1,5 +1,4 @@
 jQuery(function() {
-	console.log('universal-bar.js ready');
     jQuery("#toggle-sites-list, #toggle-notifications").on('click',function(){
 		jQuery(this).toggleClass('active-display-panel');
 	});
