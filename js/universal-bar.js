5406ca77f06d5f68dd90b0fb1ce77b617c625892
ZacDavidM <zacdavidm@gmail.com>
2014-10-27 23:36:47 -0400

    move json request outside click event

--- js/universal-bar.js
+++ js/universal-bar.js
@@ -1,20 +1,21 @@
 jQuery(function() {
-    jQuery("#toggle-sites-list, #toggle-notifications").on('click',function(){
+  jQuery("#toggle-sites-list, #toggle-notifications").on('click',function(){
 		jQuery(this).toggleClass('active-display-panel');
-    	jQuery.getJSON( "https://forgoodinc.pairsite.com/notifications/embed.json", function(response, status, jqXHR) {
-	nodes=jqXHR.responseJSON.nodes;
-  for(i=0;i<nodes.length;i++){
-    jQuery('#notifications').append(
-      '<div class="notif-ications>"',
-      nodes[i].node.nothing,
-      '</div>'
-    );
-  }
-  jQuery('#toggle-notifications > a').append(
+	});
+
+  jQuery.getJSON( "https://forgoodinc.pairsite.com/notifications/embed.json", function(response, status, jqXHR){
+    nodes=jqXHR.responseJSON.nodes;
+    for(i=0;i<nodes.length;i++){
+      jQuery('#notifications').append(
+        '<div class="notif-ications>"',
+        nodes[i].node.nothing,
+        '</div>'
+      );
+    }
+    jQuery('#toggle-notifications > a').append(
     '<span class="notification-count">',
     i,
     '</span>'
-  );
-    });
-	});
+    );
+  });
 });
